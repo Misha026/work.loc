f5e5c52feeeb08e221143c0c978e0a64f85bb871
misha777arm@gmail.com <misha777arm@gmail.com>
2023-03-29 15:13:53 +0300

    основы php

---
+++ start/функчасы.php
@@ -0,0 +1,22 @@
+<?php
+
+function days($a,$b)
+{
+    $s = $b * $a;
+    echo $b . " дюйм ". $s . " см  <br>";
+}
+
+days(2.5, 100);
+
+function hour ($a,$b)
+{
+    $s = $b / $a;
+    echo $b . " Часов ". $s . " Дней  <br>";
+}
+
+hour(24, 48);
+
+
+
+
+
