3c21d1b630a2ad4d523e0596094cda02cd48e1c2
misha777arm@gmail.com <misha777arm@gmail.com>
2023-04-03 14:26:23 +0300

    основы php

---
+++ start/функции часы.php
@@ -0,0 +1,6 @@
+<?php
+function min($a,$b){
+    $s = $b * $a;
+    echo $b." Дней " , $s . " Часов  <br>";
+}
+days( a: 24, b: 1);
--- start/функчасы.php
+++ start/функчасы.php
@@ -11,10 +11,44 @@ days(2.5, 100);
 function hour ($a,$b)
 {
     $s = $b / $a;
-    echo $b . " Часов ". $s . " Дней  <br>";
+    echo $b . " Часов  ". $s . " Дней  <br>";
 }
 
 hour(24, 48);
+
+function dney ($a,$b)
+{
+    $s = $b * $a;
+    echo $b . " Дней  ". $s . " Часов  <br>";
+}
+
+dney(24, 12);
+
+function uslovie ($a,$b)
+{
+    $s = $b / $a;
+    echo $b . " часов  ". $s . " дней  <br>";
+}
+
+uslovie(24, 48);
+
+
+
+function pl($a,$b)
+{
+    $s= $a*$b;
+    if ($s >= 1000){
+        echo 'Введите число меньше 1000';
+        exit();
+    } elseif ($s <= 0 || $b <= 0) {
+        echo 'Введите число больше 0';
+    } else {
+        echo "  " . $s ."<br> ";
+    }}
+    pl( 11,0);
+
+
+
 
 
 
